513c625d0543c391c778971ffe3e6baad9e0ac0c
Yousef Khaled <117599930+YousefKhaled53@users.noreply.github.com>
2022-12-09 00:27:52 +0200

    12

---
+++ IrrPolygon.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "shapes/Shape.h"
+class IrrPolygon :
+    public shape
+{
+	private:
+		Point Corner1;
+		Point Corner2;
+	public:
+		IrrPolygon(Point, Point, GfxInfo shapeGfxInfo);
+		virtual ~IrrPolygon();
+		virtual void Draw(GUI* pUI) const;
+	};
+
+};
+
