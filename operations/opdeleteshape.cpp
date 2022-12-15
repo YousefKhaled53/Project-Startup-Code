060401e15f4c075c23aa623004745bfa942b6ad0
abdulrahman aboshareb <117599976+Abdoaboshareb@users.noreply.github.com>
2022-12-16 00:33:49 +0200

    Update opdeleteshape.cpp

--- operations/opdeleteshape.cpp
+++ operations/opdeleteshape.cpp
@@ -6,7 +6,6 @@
 opdeleteshape::opdeleteshape(controller* pcont) :operation(pcont) {};
 void opdeleteshape::Execute() {
 	GUI* pUI = pControl->GetUI(); // getting a pointer from gui so that i can call print message and call selectshape
-	pUI->PrintMessage("choose the shape ");
 	Graph* pGr = pControl->getGraph(); // getting a pointer of grapg so that i can call delete shape and getshape (but what does getshape do ??)
 	pGr->deleteshape(pGr->GetSelected()); // is done after making select function 
 	
