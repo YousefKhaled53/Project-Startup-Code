03007c49c70f65ce8db88e140fdceaeda6bfed9c
Belal74 <117991361+Belal74@users.noreply.github.com>
2022-12-16 19:26:06 +0200

    Adding Line, Circle, and Edit to Rect, Square selection

--- Shapes/line.cpp
+++ Shapes/line.cpp
@@ -1,6 +1,8 @@
 #include "line.h"
 #include<iostream>
 #include<fstream>
+#include <cmath>
+#include <corecrt_math_defines.h>
 line::line(Point P1, Point P2, GfxInfo shapeGfxInfo) :shape(shapeGfxInfo)
 {
 	Corner1 = P1;
@@ -26,7 +28,10 @@ void line::Save(ofstream& OutFile) {
 	OutFile << "///" << " ";
 }
 bool line::is_in_fig(int x, int y) {
-	//if ((x > Corner1.x && x < Corner2.x && y > Corner1.y && y < Corner2.y))
+	//int LineSlope = ((Corner2.y - Corner1.y) / (Corner2.x - Corner1.x));
+	////double LineLength = sqrt(pow((Corner1.x - Corner2.x), 2) + pow((Corner1.y - Corner2.y), 2));
+	//int Y_Intercept = (Corner1.y -(LineSlope * Corner1.x));
+	//if ((y == (LineSlope * x) + Y_Intercept) /*&& min(Corner1.x, Corner2.x) <= x && x <= max(Corner1.x, Corner2.x)*/)
 	//{
 	//	return true;
 	//}
@@ -34,5 +39,14 @@ bool line::is_in_fig(int x, int y) {
 	//{
 	//	return false;
 	//}
-	return false;
+	int LineLength = sqrt(pow((Corner1.x - Corner2.x), 2) + pow((Corner1.y - Corner2.y), 2));
+	int Line1 = sqrt(pow((Corner1.x - x), 2) + pow((Corner1.y - y), 2));
+	int Line2= sqrt(pow((x - Corner2.x), 2) + pow((y - Corner2.y), 2));
+	if (Line1 + Line2 == LineLength) {
+		return true;
+
+	}
+	else {
+		return false;
+	}
 }
