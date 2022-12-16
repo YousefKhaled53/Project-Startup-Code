8ba79594e56ad491b5a26ab893ea85260a6b6981
abdulrahman aboshareb <117599976+Abdoaboshareb@users.noreply.github.com>
2022-12-16 23:06:16 +0200

    pallete is working

--- operations/Opchangefillcolor.cpp
+++ operations/Opchangefillcolor.cpp
@@ -8,8 +8,11 @@ void Opchangefillcolor::Execute() {
 	GUI* pUI = pControl->GetUI();
 	Graph* pGr = pControl->getGraph();
 	Point p1;
+	pUI->getwind()->SetPen(BLACK );
+	pUI->getwind()->DrawString(300,60,"the pallete ");
+	pUI->getwind()->DrawImage("images\\MenuIcons\\p.jpg",300,100);
 	pUI->PrintMessage("click on the needed color ");
 	pUI->GetPointClicked(p1.x, p1.y);
-	color c2=pUI->getwind()->GetColor(p1.x,p1.y);
+	color c2=pUI->getwind()->GetColor(p1.x, p1.y);
 	pGr->changefillcolor(pGr->GetSelected(),c2);
 }
