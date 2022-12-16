a578d15ed212a9214b52049f13074bd0694aab8b
Yousef Khaled <117599930+YousefKhaled53@users.noreply.github.com>
2022-12-16 18:44:04 +0200

    asking for saving yes or no

--- operations/opsave.h
+++ operations/opsave.h
@@ -12,11 +12,13 @@ public:
 	~opsave() {}
 	virtual void Execute() {
 		GUI* pUI = pControl->GetUI();
-		pUI->PrintMessage("write file name, press esc to cancel ");
+		pUI->PrintMessage("write file name , write cancel to cancel ");
 		string name =pUI->GetSrting();
-		ofstream savingfile(name); // shouldn't get string return a string to be used by the program ? why does it use printmessage function
-		pControl->getGraph()->Save(savingfile); // how to call save all function 
-		pUI->PrintMessage("saved");
+		if (name != "cancel") {
+			ofstream savingfile(name); // shouldn't get string return a string to be used by the program ? why does it use printmessage function
+			pControl->getGraph()->Save(savingfile); // how to call save all function 
+			pUI->PrintMessage("saved");
+		}
 	};
 
 };
