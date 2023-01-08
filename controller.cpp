51cc6f90b6ffb7d43b690c71f9df7dba432af0c1
abdulrahman aboshareb <117599976+Abdoaboshareb@users.noreply.github.com>
2023-01-08 22:42:32 +0200

    Revert "yarab"
    This reverts commit 8146a5778e91ccdd4fd5ad85c1c29887bdbb9117.

--- controller.cpp
+++ controller.cpp
@@ -74,6 +74,15 @@ operation* controller::createOperation(operationType OpType)
 	switch (OpType)
 	{
 		case DRAW_LINE:
+<<<<<<< Updated upstream
+=======
+			//pOp = new ophide(this);
+			//pOp = new opdublicate(this);
+			//pOp = new opundo(this);
+
+			//pOp = new opStickImage(this);
+			//pOp = new opCancelFillingGeneral(this);
+>>>>>>> Stashed changes
 			pOp = new opAddline(this);
 			break;
 		case DRAW_RECT:
@@ -108,6 +117,7 @@ operation* controller::createOperation(operationType OpType)
 			break;
 		case BRUSH_GENERAL: 
 			pOp = new opChangeGeneralFillColor(this);
+			
 			break;
 		case BORDER_SINGLE: 
 			pOp = new opborderwidth(this);
@@ -195,12 +205,16 @@ operation* controller::createOperation(operationType OpType)
 		case redo:
 			pOp = new opredo(this);
 			break;
+<<<<<<< Updated upstream
 		case stick:
 			pOp = new opStickImage(this);
+=======
 
 		case cut:
 			break;
-
+		case stick:
+>>>>>>> Stashed changes
+			break;
 	}
 
 	return pOp;
